35c30de861be9697980bc18bab9a90707f688aa0
Hunter Garrett <hunter@hunterbecton.com>
2026-05-04 20:27:57 -0400

    chore(matter-cli): add commander runtime dep and __VERSION__ build-time inject

--- packages/matter-cli/tsup.config.ts
+++ packages/matter-cli/tsup.config.ts
@@ -1,4 +1,9 @@
+import { readFileSync } from 'node:fs'
 import { defineConfig } from 'tsup'
+
+const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf-8')) as {
+  version: string
+}
 
 export default defineConfig({
   entry: ['src/index.ts'],
@@ -7,4 +12,7 @@ export default defineConfig({
   sourcemap: true,
   clean: true,
   banner: { js: '#!/usr/bin/env node' },
+  define: {
+    __VERSION__: JSON.stringify(pkg.version),
+  },
 })
