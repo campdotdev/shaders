79a3d2b4c4f5cb1b4851ab095e23d10c531e8c20
Hunter Garrett <hunter@hunterbecton.com>
2026-06-12 22:49:55 -0400

    refactor(registry): clarify variable names in aurora shader

--- registry/aurora/shader.tsx
+++ registry/aurora/shader.tsx
@@ -54,15 +54,15 @@ interface LayerUniforms {
 }
 
 function useLayerUniforms(layer: AuroraLayer): LayerUniforms {
-  const speedU = useAnimatableUniform<number>(layer.speed);
-  const intensityU = useAnimatableUniform<number>(layer.intensity);
-  const falloffU = useAnimatableUniform<number>(layer.falloff ?? 1);
+  const speedUniform = useAnimatableUniform<number>(layer.speed);
+  const intensityUniform = useAnimatableUniform<number>(layer.intensity);
+  const falloffUniform = useAnimatableUniform<number>(layer.falloff ?? 1);
 
   const colorVec = useMemo(
     () => {
-      const [r, g, b] = parseHex(layer.hex);
+      const [redChannel, greenChannel, blueChannel] = parseHex(layer.hex);
 
-      return new Vector3(r, g, b);
+      return new Vector3(redChannel, greenChannel, blueChannel);
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [],
@@ -71,9 +71,9 @@ function useLayerUniforms(layer: AuroraLayer): LayerUniforms {
   const colorNode = useMemo(() => uniform(colorVec), [colorVec]);
 
   useEffect(() => {
-    const [r, g, b] = parseHex(layer.hex);
+    const [redChannel, greenChannel, blueChannel] = parseHex(layer.hex);
 
-    colorVec.set(r, g, b);
+    colorVec.set(redChannel, greenChannel, blueChannel);
   }, [layer.hex, colorVec]);
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
@@ -82,20 +82,20 @@ function useLayerUniforms(layer: AuroraLayer): LayerUniforms {
   return useMemo(
     () => ({
       color: widenedColor,
-      speed: speedU,
-      intensity: intensityU,
-      falloff: falloffU,
+      speed: speedUniform,
+      intensity: intensityUniform,
+      falloff: falloffUniform,
     }),
-    [widenedColor, speedU, intensityU, falloffU],
+    [widenedColor, speedUniform, intensityUniform, falloffUniform],
   );
 }
 
 function useColorUniform(hex: string) {
   const vec = useMemo(
     () => {
-      const [r, g, b] = parseHex(hex);
+      const [redChannel, greenChannel, blueChannel] = parseHex(hex);
 
-      return new Vector3(r, g, b);
+      return new Vector3(redChannel, greenChannel, blueChannel);
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [],
@@ -104,45 +104,49 @@ function useColorUniform(hex: string) {
   const node = useMemo(() => uniform(vec), [vec]);
 
   useEffect(() => {
-    const [r, g, b] = parseHex(hex);
+    const [redChannel, greenChannel, blueChannel] = parseHex(hex);
 
-    vec.set(r, g, b);
+    vec.set(redChannel, greenChannel, blueChannel);
   }, [hex, vec]);
 
   return node;
 }
 
 export function AuroraShader(props: AuroraShaderProps) {
-  const ctx = useShaderContext();
+  const shaderContext = useShaderContext();
   const resize = useResize();
 
-  const intensityU = useAnimatableUniform<number>(props.intensity);
-  const speedU = useAnimatableUniform<number>(props.speed);
-  const densityXU = useAnimatableUniform<number>(props.densityX);
-  const densityYU = useAnimatableUniform<number>(props.densityY);
-  const falloffU = useAnimatableUniform<number>(props.falloff);
-  const driftXU = useAnimatableUniform<number>(props.driftX);
-  const driftYU = useAnimatableUniform<number>(props.driftY);
-  const turbulenceU = useAnimatableUniform<number>(props.turbulence);
+  const intensityUniform = useAnimatableUniform<number>(props.intensity);
+  const speedUniform = useAnimatableUniform<number>(props.speed);
+  const densityXUniform = useAnimatableUniform<number>(props.densityX);
+  const densityYUniform = useAnimatableUniform<number>(props.densityY);
+  const falloffUniform = useAnimatableUniform<number>(props.falloff);
+  const driftXUniform = useAnimatableUniform<number>(props.driftX);
+  const driftYUniform = useAnimatableUniform<number>(props.driftY);
+  const turbulenceUniform = useAnimatableUniform<number>(props.turbulence);
 
-  const [iw, ih] = resize.get();
-  const aspectNode = useMemo(() => uniform(ih > 0 ? iw / ih : 16 / 9), []); // eslint-disable-line react-hooks/exhaustive-deps
+  const [initialWidth, initialHeight] = resize.get();
+  const aspectNode = useMemo(
+    () => uniform(initialHeight > 0 ? initialWidth / initialHeight : 16 / 9),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
 
   useEffect(() => {
-    const [w, h] = resize.get();
+    const [canvasWidth, canvasHeight] = resize.get();
 
-    if (w > 0 && h > 0) aspectNode.value = w / h;
+    if (canvasWidth > 0 && canvasHeight > 0) aspectNode.value = canvasWidth / canvasHeight;
 
-    return resize.on('change', ([w2, h2]) => {
-      if (w2 > 0 && h2 > 0) aspectNode.value = w2 / h2;
+    return resize.on('change', ([updatedWidth, updatedHeight]) => {
+      if (updatedWidth > 0 && updatedHeight > 0) aspectNode.value = updatedWidth / updatedHeight;
     });
   }, [resize, aspectNode]);
 
   const dirVec = useMemo(
     () => {
-      const [x, y, b] = DIRECTION_VECTORS[props.direction];
+      const [directionX, directionY, directionBias] = DIRECTION_VECTORS[props.direction];
 
-      return new Vector3(x, y, b);
+      return new Vector3(directionX, directionY, directionBias);
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [],
@@ -151,9 +155,9 @@ export function AuroraShader(props: AuroraShaderProps) {
   const dirNode = useMemo(() => uniform(dirVec), [dirVec]);
 
   useEffect(() => {
-    const [x, y, b] = DIRECTION_VECTORS[props.direction];
+    const [directionX, directionY, directionBias] = DIRECTION_VECTORS[props.direction];
 
-    dirVec.set(x, y, b);
+    dirVec.set(directionX, directionY, directionBias);
   }, [props.direction, dirVec]);
 
   const horizonNode = useColorUniform(props.horizonColor);
@@ -184,52 +188,65 @@ export function AuroraShader(props: AuroraShaderProps) {
     const material = new MeshBasicNodeMaterial();
 
     const aspect = aspectNode;
-    const scaledUv = vec2(uv().x.mul(aspect).mul(densityXU), uv().y.mul(densityYU));
+    const scaledUv = vec2(uv().x.mul(aspect).mul(densityXUniform), uv().y.mul(densityYUniform));
 
     const fallOff = uv().x.mul(dirNode.x).add(uv().y.mul(dirNode.y)).add(dirNode.z);
 
     let aurora = vec3(0, 0, 0);
 
-    for (let i = 0; i < 4; i += 1) {
-      const lu = layerUniforms[i];
-      const variation = variations[i];
+    for (let layerIndex = 0; layerIndex < 4; layerIndex += 1) {
+      const layerUniform = layerUniforms[layerIndex];
+      const variation = variations[layerIndex];
 
-      if (lu === undefined || variation === undefined) continue;
+      if (layerUniform === undefined || variation === undefined) continue;
 
-      const t = time.mul(speedU).mul(lu.speed);
+      const scaledTime = time.mul(speedUniform).mul(layerUniform.speed);
 
-      const p = vec2(scaledUv.x.add(t.mul(driftXU)), scaledUv.y.add(t.mul(driftYU)));
+      const driftPosition = vec2(
+        scaledUv.x.add(scaledTime.mul(driftXUniform)),
+        scaledUv.y.add(scaledTime.mul(driftYUniform)),
+      );
 
-      const warpSeed = vec2(lu.color.x.add(variation), lu.color.y.add(variation + 1));
+      const warpSeed = vec2(
+        layerUniform.color.x.add(variation),
+        layerUniform.color.y.add(variation + 1),
+      );
 
-      const inner = noise(vec2(warpSeed.x.add(p.x).add(t), warpSeed.y.add(p.y).add(t)))
+      const warpOffset = noise(
+        vec2(
+          warpSeed.x.add(driftPosition.x).add(scaledTime),
+          warpSeed.y.add(driftPosition.y).add(scaledTime),
+        ),
+      )
         .mul(0.5)
         .add(0.5)
-        .mul(turbulenceU);
+        .mul(turbulenceUniform);
 
-      const n = noise(vec2(p.x.add(inner), p.y.add(inner)))
+      const noiseValue = noise(
+        vec2(driftPosition.x.add(warpOffset), driftPosition.y.add(warpOffset)),
+      )
         .mul(0.5)
         .add(0.5);
 
-      const auroraField = n.sub(fallOff.mul(falloffU).mul(lu.falloff));
+      const auroraField = noiseValue.sub(fallOff.mul(falloffUniform).mul(layerUniform.falloff));
 
-      aurora = aurora.add(lu.color.mul(auroraField).mul(lu.intensity).mul(2));
+      aurora = aurora.add(layerUniform.color.mul(auroraField).mul(layerUniform.intensity).mul(2));
     }
 
     const sky = horizonNode
       .mul(sub(1, smoothstep(0, 0.5, fallOff)))
       .add(skyNode.mul(sub(1, smoothstep(0.4, 1, fallOff))));
 
-    const finalColor = sky.add(aurora.mul(intensityU));
+    const finalColor = sky.add(aurora.mul(intensityUniform));
 
     material.colorNode = vec4(finalColor, 1);
 
     const mesh = new Mesh(new PlaneGeometry(2, 2), material);
 
-    ctx?.scene.add(mesh);
+    shaderContext?.scene.add(mesh);
 
     return () => {
-      ctx?.scene.remove(mesh);
+      shaderContext?.scene.remove(mesh);
       try {
         material.dispose();
       } catch {
@@ -237,17 +254,17 @@ export function AuroraShader(props: AuroraShaderProps) {
       }
     };
   }, [
-    ctx,
+    shaderContext,
     layerUniforms,
     variations,
-    intensityU,
-    speedU,
-    densityXU,
-    densityYU,
-    falloffU,
-    driftXU,
-    driftYU,
-    turbulenceU,
+    intensityUniform,
+    speedUniform,
+    densityXUniform,
+    densityYUniform,
+    falloffUniform,
+    driftXUniform,
+    driftYUniform,
+    turbulenceUniform,
     horizonNode,
     skyNode,
     aspectNode,
