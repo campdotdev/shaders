9c350d5402430861f7513c6929f173879ccac190
Hunter Garrett <hunter@hunterbecton.com>
2026-06-12 22:49:55 -0400

    refactor(registry): clarify variable names in color util

--- registry/utils/color.ts
+++ registry/utils/color.ts
@@ -1,9 +1,9 @@
 export const parseHex = (hex: string): [number, number, number] => {
-  const c = hex.replace('#', '');
+  const cleanedHex = hex.replace('#', '');
 
   return [
-    parseInt(c.slice(0, 2), 16) / 255,
-    parseInt(c.slice(2, 4), 16) / 255,
-    parseInt(c.slice(4, 6), 16) / 255,
+    parseInt(cleanedHex.slice(0, 2), 16) / 255,
+    parseInt(cleanedHex.slice(2, 4), 16) / 255,
+    parseInt(cleanedHex.slice(4, 6), 16) / 255,
   ];
 };
